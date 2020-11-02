4a61e0047541db564b4df75218481fcf3fed59ff
ariasDev <57456319+ariasDev@users.noreply.github.com>
2020-11-01 19:39:03 -0500

    logica de componente login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  checkoutForm: FormGroup;
+  private suscribeGet: Subscription;
+  private suscribePost: Subscription;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)])
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(userData):void {
+    console.log('entra en el onSubmit');
+    console.log('---Data: ', userData);
+    
+  }
+
+  get email() { return this.checkoutForm.get('email') }
+  get password() { return this.checkoutForm.get('password') }
+
+  ngOnDestroy() {
+    this.suscribeGet.unsubscribe()
+    this.suscribePost.unsubscribe()
   }
 
 }
