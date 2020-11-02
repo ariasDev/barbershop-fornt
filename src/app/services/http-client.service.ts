fadaee6cc0ea72307d5770ffb9a368c0fd57bb4f
ariasDev <57456319+ariasDev@users.noreply.github.com>
2020-11-01 19:49:19 -0500

    add routes httpClient service

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
+  private URL_BASE = 'http://localhost:3001'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public login(body) {
+    return this.http.post(`${this.URL_BASE}/login`, body);
+  }
+
+  public registry(body) {
+    return this.http.post(`${this.URL_BASE}/registry`, body)
+  }
 }
