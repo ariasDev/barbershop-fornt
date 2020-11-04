3c3f61ffaf83f9a978c388debfe1345c0e28b9dc
alejopalaciog <alejopalaciog@gmail.com>
2020-11-04 13:39:13 -0500

    asdasdfas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'barberfront';
+  
+  constructor(private router: Router){
+  }
+  
+  logout(){
+    localStorage.setItem("userType",null) 
+  }
+
+  OnRedirect(){
+    location.reload()
+  }
+
+  validateUserType(): boolean {
+    return localStorage.getItem("userType")==="admin" ? true : false;
+  }
+
+  currentUser(): boolean {    
+    return localStorage.getItem("userType") != null ? true : false;
+  }
+
 }
