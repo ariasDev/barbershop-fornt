7b3a25be47ff4115e64abc6eae38889d340e9c29
alejopalaciog <alejopalaciog@gmail.com>
2020-11-04 14:54:10 -0500

    asdasd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MainComponent } from './components/main/main.component';
+import { AuthenticateGuard } from './guards/authenticate.guard';
 
 const routes: Routes = [
   { path: '',   component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
-  { path: 'create-barber', component: CreateBarberComponent },
+  { path: 'create-barber', component: CreateBarberComponent,canActivate:[AuthenticateGuard] },
 
-  { path: 'main', component: MainComponent }
+  { path: 'main', component: MainComponent,canActivate:[AuthenticateGuard] }
 
 ];
 
---
+++ src/app/guards/authenticate.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate,  Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateGuard implements CanActivate {
+  constructor( private router: Router){ }
+
+  currentUser(): boolean {
+    if(localStorage.getItem("userType") === null 
+    || localStorage.getItem("userType") === 'null'
+    || localStorage.getItem("userType") === undefined ){
+      console.log(false)
+      return false;
+    }else{
+      console.log(true)
+      return true;
+    }  
+  }
+
+  canActivate(){
+    if(this.currentUser()){
+      return true;
+    }else{
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+  
+}
